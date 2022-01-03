21c11bc7d1ce5005a7524bea8fb99b90426e8ee2
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-03 13:39:00 -0700

    added read

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -4,6 +4,16 @@ function listAllTables() {
   return knex('tables').select('*').orderBy('table_name');
 }
 
+function readIndividualTable(id) {
+  return knex('tables').select('*').where({ table_id: id }).first();
+}
+// for validation
+function readIndividualReservation(id) {
+  return knex('reservations').select('*').where({ reservation_id: id }).first();
+}
+
 module.exports = {
   list: listAllTables,
+  read: readIndividualTable,
+  readReservation: readIndividualReservation,
 };
