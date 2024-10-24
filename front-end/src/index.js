2f668139eff786f50b1a2c6600819222e2afe6eb
jordanmowry <jordan.mowry@gmail.com>
2024-10-24 14:38:04 -0400

    fix: refactor code

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext/ThemeContext';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <Router>
         <App />
       </Router>
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
